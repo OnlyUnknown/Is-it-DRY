b01cab030bbd3e33930454c9e4ec3eb24e572938
Abdullah <abdullahkhrief@gmail.com>
2023-03-06 18:50:26 +0300

    the second example

---
+++ Example 2/JS.js
@@ -0,0 +1,11 @@
+const greet = (message, name) => {
+    console.log(`${message}, ${name}!`)
+  }
+  
+  greet('Hello', 'John');
+  greet('Hola', 'Antonio');
+  greet('Ciao', 'Luigi')
+
+
+//   it uses the arrow function to make it more shorter 
+//   and there is no posibility to make it more clean
